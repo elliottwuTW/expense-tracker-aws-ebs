b129f1d869e53edb95ca4b2c4193df3010f73d88
elliottwuTW <elliott10009@gmail.com>
2020-10-19 10:26:22 +0800

    feat: add express-handlebars and bootstrap
    建立符合 express-handlebars 規範的檔案目錄
    簡單的建構 index.hbs(主頁)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // modules
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 // app
 const app = express()
@@ -8,10 +9,23 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // middleware
+app.engine('hbs', exphbs({
+  defaultLayout: 'layout',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
 
 // routing
 app.get('/', (req, res) => {
-  res.send('hello!')
+  const records = [
+    {
+      category: '餐飲',
+      name: '午餐',
+      date: '2020/10/16',
+      amount: 50
+    }
+  ]
+  res.render('index', { totalAmount: 1000, records })
 })
 
 // start and listen to the server
