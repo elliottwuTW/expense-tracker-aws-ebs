7cffea772aeabda9379dcf8be1b4a631975e3924
elliottwuTW <elliott10009@gmail.com>
2021-03-10 21:15:09 +0800

    test local login

--- config/passport.js
+++ config/passport.js
@@ -34,20 +34,21 @@ module.exports = (app) => {
     new LocalStrategy(
       { usernameField: 'email', passReqToCallback: true },
       (req, email, password, done) => {
-        User.findOne({ email })
-          .then((user) => {
-            if (!user) {
-              return done(null, false, req.flash('login_error', 'User not found'))
-            }
+        return done(null, { id: 'test-id', name: 'test-user', email }, req.flash('login_success', 'Login successfully'))
+        // User.findOne({ email })
+        //   .then((user) => {
+        //     if (!user) {
+        //       return done(null, false, req.flash('login_error', 'User not found'))
+        //     }
 
-            bcrypt.compare(password, user.password).then((isMatch) => {
-              if (!isMatch) {
-                return done(null, false, req.flash('login_error', 'Incorrect password'))
-              }
-              return done(null, user, req.flash('login_success', 'Login successfully'))
-            })
-          })
-          .catch((err) => done(err, null))
+        //     bcrypt.compare(password, user.password).then((isMatch) => {
+        //       if (!isMatch) {
+        //         return done(null, false, req.flash('login_error', 'Incorrect password'))
+        //       }
+        //       return done(null, user, req.flash('login_success', 'Login successfully'))
+        //     })
+        //   })
+        //   .catch((err) => done(err, null))
       }
     )
   )
