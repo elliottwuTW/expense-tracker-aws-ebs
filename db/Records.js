2eaee2e46eac23c64648bab8add940d9e8ad850f
elliottwuTW <elliott10009@gmail.com>
2021-03-12 11:37:18 +0800

    debug

--- db/Records.js
+++ db/Records.js
@@ -1,7 +1,7 @@
 const dynamodb = require('./dynamodb')
 
 exports.getRecordsByGSI = (params) => {
-  params.TableName = process.env.RECORD_TABLE_NAME
-  params.IndexName = process.env.RECORD_TABLE_GSI_NAME
+  // params.TableName = process.env.RECORD_TABLE_NAME
+  // params.IndexName = process.env.RECORD_TABLE_GSI_NAME
   return dynamodb.query(params).promise()
 }
