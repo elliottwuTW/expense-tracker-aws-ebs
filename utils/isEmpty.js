aba9f5abab7444f216d4ee2874105d736fd0757e
elliottwuTW <elliott10009@gmail.com>
2021-03-12 10:49:46 +0800

    debug

--- utils/isEmpty.js
+++ utils/isEmpty.js
@@ -1,5 +1,5 @@
 module.exports = (object) => {
-  console.log('object: ', oject)
+  console.log('object: ', object)
   // Array
   if (Array.isArray(object) || typeof object === 'string' || object instanceof String) return object.length === 0
   // Object
